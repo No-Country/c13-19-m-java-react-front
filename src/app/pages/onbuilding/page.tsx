ace7e394ccfc75cccd3aa4e5b7b6af9cecd95336
PaulaVelezz <velezpaula.a@gmail.com>
2023-09-01 00:55:38 -0300

    optimizacion de imagenes y error page

--- src/app/pages/onbuilding/page.tsx
+++ src/app/pages/onbuilding/page.tsx
@@ -2,44 +2,48 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function OnBuilding() {
-    return (
-        <div className="container min-h-screen p-6 mx-auto lg:flex lg:items-center">
-            <div className="flex flex-col items-center w-full lg:w-1/2">
-                <h1 className="text-purple-500 font-semibold text-4xl p-5 text-center">
-                Proxímamente grandes novedades
-                </h1>
-                <p className="text-gray-600 font-normal text-lg text-center">
-                Estamos construyendo algo nuevo para ti, sé el primero en enterarte.
-                </p>
+  return (
+    <div className="container min-h-screen p-6 mx-auto lg:flex lg:items-center">
+      <div className="flex flex-col items-center w-full lg:w-1/2">
+        <h1 className="text-purple-500 font-semibold text-4xl p-5 text-center">
+          Próximamente grandes novedades
+        </h1>
+        <p className="text-gray-600 font-normal text-lg text-center">
+          Estamos construyendo algo nuevo para ti, sé el primero en enterarte.
+        </p>
 
-                <div className="flex flex-col p-1 mt-6 rounded border border-slate-500 space-y-3 lg:space-y-0 lg:flex-row">
-                    <input 
-                        type="text"
-                        placeholder="Ingrese un correo"
-                        className="text-center bg-transparent "
-                    />
+        <div className="flex flex-col p-1 mt-6 rounded border border-slate-500 space-y-3 lg:space-y-0 lg:flex-row">
+          <input
+            type="text"
+            placeholder="Ingrese un correo"
+            className="text-center bg-transparent m-1"
+            required
+          />
 
-                    <button className="text-1xl tracking-wider font-medium px-5 py-2 text-white bg-purple-500 hover:bg-purple-600 rounded">
-                        Notifícarme
-                    </button>
-                </div>
-
-                <Link 
-                    href={"/"}
-                    className="font-bold m-6 px-5 py-2 text-sm text-center tracking-wide text-slate-600 bg-slate-50 hover:text-white hover:border-purple-500 hover:bg-slate-700 border rounded-lg border-slate-500"
-                >
-                    Ir al inicio
-                </Link>
-            </div>
-
-            <div className="flex justify-center w-full lg:w-1/2">
-                <Image
-                    alt="coming-soon"
-                    src={"/images/coming.png"}
-                    width={400}
-                    height={400}
-                />
-            </div>
+          <button
+            className="text-1xl tracking-wider font-medium px-5 py-2 text-white bg-purple-500 hover:bg-purple-600 rounded"
+            //*completar con backend//
+          >
+            Notificarme
+          </button>
         </div>
-    );
+
+        <Link
+          href={"/"}
+          className="font-bold m-6 px-5 py-2 text-sm text-center tracking-wide text-slate-600 bg-slate-50 hover:text-white hover:border-purple-500 hover:bg-slate-700 border rounded-lg border-slate-500"
+        >
+          Ir al inicio
+        </Link>
+      </div>
+
+      <div className="flex justify-center w-full lg:w-1/2">
+        <Image
+          alt="coming-soon"
+          src={"/images/coming.webp"}
+          width={400}
+          height={400}
+        />
+      </div>
+    </div>
+  );
 }
