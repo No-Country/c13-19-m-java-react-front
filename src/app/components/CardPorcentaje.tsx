a14a468f7b87e3c5a26b1665284dd45b47307bb6
danielghost20 <danielramosmartinez72@gmail.com>
2023-09-15 08:04:22 -0600

    resolve conflict cardPorcentaje

--- src/app/components/CardPorcentaje.tsx
+++ src/app/components/CardPorcentaje.tsx
@@ -2,6 +2,15 @@ import Image from "next/image";
 import "tailwindcss/tailwind.css";
 import "daisyui/dist/full.css";
 import CardCtainerProgress from "@/app/components/CardCtainerProgress";
+import './card.css'
+
+declare module 'react' {
+  interface CSSProperties {
+    '--value'?: number;
+    '--size'?: string;
+    '--thickness'?: string
+  }
+}
 
 interface CardPorcentaje {
   porcentaje: number;
